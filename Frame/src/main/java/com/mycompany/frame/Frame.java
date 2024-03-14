ded61a5ebd2dd73862c55f61c971b71d8bbd8492
Erick Daniel Teixeira Vier <erixkat10@gmail.com>
2024-03-13 22:19:34 -0300

    Add files via upload

---
+++ Frame/src/main/java/com/mycompany/frame/Frame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.frame;
+
+/**
+ *
+ * @author aluno
+ */
+public class Frame {
+
+    public static void main(String[] args) {
+        new FrameCadastro().setVisible(true);
+    }
+}
